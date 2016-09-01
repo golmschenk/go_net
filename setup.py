8b3543dccd173ac7e75e9f630f7e5258c7bd5151
Greg Olmschenk <golmschenk@gmail.com>
2016-09-01 22:02:22 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+The installer file.
+"""
+
+from setuptools import setup
+
+setup(
+    name='gonet',
+    version='1.0',
+    description='The Go Net.',
+    url='https://github.com/golmschenk/go_net',
+    license='MIT',
+    packages=['gonet'],
+)
