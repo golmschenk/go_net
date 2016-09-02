bf3e43b082f764be6ac727ff8240935833c965d8
Greg Olmschenk <golmschenk@gmail.com>
2016-09-02 11:23:25 +0200

    Update version number again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='gonet',
-    version='1.0.0',
+    version='0.1.0',
     description='The Go Net.',
     url='https://github.com/golmschenk/go_net',
     license='MIT',
